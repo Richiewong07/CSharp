764160f2aea7ad04622ed23be29a872844013681
Richie Wong <richiewong07@yahoo.com>
2018-05-15 15:13:10 -0500

    updated

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -6,7 +6,34 @@ namespace Operators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			//var a = 10;
+			//var b = 3;
+			//Console.WriteLine(a / b);
+
+
+			// FLOATING POINT NUMBERS
+			//var a = 10;
+			//var b = 3;
+			//Console.WriteLine((float)a / (float)b);
+
+
+			// OPERATOR PRECEDENCE
+			//var a = 1;
+			//var b = 2;
+			//var c = 3;
+			//Console.WriteLine(a + b * c);
+
+
+			//// COMPARION OPERATORS -> GIVES BOOLEAN VALUE
+			//var a = 1;
+			//var b = 2;
+			//Console.WriteLine(!(a != b)); // CONVERT TWO NEGATIVEES(!) TO POSITIVE --> (a == b)
+
+			// LOGICAL OPERATORS
+			var a = 1;
+			var b = 2;
+			var c = 3;
+			Console.WriteLine(c > b && c > a);
         }
     }
 }
