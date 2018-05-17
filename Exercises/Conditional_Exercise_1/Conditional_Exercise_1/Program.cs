2bcdd724e830dc37b87efd3c5c465186afed57f3
Richie Wong <richiewong07@yahoo.com>
2018-05-17 16:26:57 -0500

    updated

--- Exercises/Conditional_Exercise_1/Conditional_Exercise_1/Program.cs
+++ Exercises/Conditional_Exercise_1/Conditional_Exercise_1/Program.cs
@@ -6,7 +6,25 @@ namespace Conditional_Exercise_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /// <summary>
+            /// Write a program and ask the user to enter a number. The number should be between 1 to 10. If the user enters 
+            /// a valid number, display "Valid" on the console. Otherwise, display "Invalid". (This logic is used a lot in 
+            /// applications where values entered into input boxes need to be validated.)
+            /// </summary>
+
+            Console.Write("Enter a number between 1 and 10: ");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+            if (number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+
         }
+
     }
 }
