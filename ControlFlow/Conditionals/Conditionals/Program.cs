b73f33593936d3c51728108fea638e45309d0158
Richie Wong <richiewong07@yahoo.com>
2018-05-17 15:57:32 -0500

    updated

---
+++ ControlFlow/Conditionals/Conditionals/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Conditionals
+{
+	class Program
+    {
+        static void Main(string[] args)
+        {
+
+			// SWITCH CASE
+			var season = Season.Autumn;
+            
+            switch (season)
+			{
+				case Season.Autumn:
+				case Season.Summer:
+					Console.WriteLine("We got a promotion");
+					break;
+
+				//case Season.Autumn:
+				//	Console.WriteLine("It's autumn and a beatiful season.");
+				//	break;
+                 
+				//case Season.Summer:
+					//Console.WriteLine("It's perfect to go to the beach.");
+					//break;
+
+				default:
+					Console.WriteLine("I don't understand that season!");
+					break;
+			}
+
+
+           
+
+			// CONDITIONAL OPERATORS
+			//float price = (isGoldCustomer) ? 19.95f : 29.95;
+			//Console.WriteLine(price);
+
+			//bool isGoldCustomer = true;
+
+            //float price;
+
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+                //price = 29.95f;
+
+           
+
+            // IF/ELSE STATEMENT                             
+			//int hour = 10;
+
+			//if (hour > 0 && hour < 12)
+			//{
+			//	Console.WriteLine("It's morning.");
+			//}
+			//else if (hour >= 12 && hour < 18)
+			//{
+			//	Console.WriteLine("It's afternoon.");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("It's evening.");
+			//}
+
+		}
+    }
+}
---
+++ NonPrimitiveTypes/ReferenceAndValueTypes/ReferenceAndValueTypes/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ReferenceAndValueTypes
+{
+    public class Person
+	{
+		public int Age;
+	}
+
+
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			var number = 1;
+			Increment(number);
+            Console.WriteLine(number); // 1
+
+			var person = new Person() { Age = 20 };
+			MakeOld(person);
+			Console.WriteLine(person.Age); // 30
+		}
+
+
+		public static void Increment(int number)
+		{
+			number += 10;
+            Console.WriteLine(number); // 11
+		}
+
+
+        public static void MakeOld(Person person)
+		{
+			person.Age += 10;
+            
+		}
+
+        
+
+       
+			//// VALUE TYPES
+			//var a = 10;
+			//var b = a;
+			//b++;
+			//Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
+            
+			//// REFERENCE TYPES
+			//var arr1 = new int[3] { 1, 2, 3 };
+			//var arr2 = arr1;
+			//arr2[0] = 0;
+			//Console.WriteLine(string.Format("arr1[0]: {0}, arr2[0]: {1}", arr1[0], arr2[0]));
+
+
+    }
+}
