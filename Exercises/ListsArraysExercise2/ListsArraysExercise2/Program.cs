6288d457a2857841e7d7cb7cbb850457f4e18ef1
Richie Wong <richiewong07@yahoo.com>
2018-05-18 20:32:01 -0500

    new

---
+++ Exercises/ListsArraysExercise2/ListsArraysExercise2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ListsArraysExercise2
+{
+    class Program
+    {
+		/// <summary>
+        /// Ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. 
+        /// Display the reversed name on the console.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+			
+			//Console.Write("What is your name: ");
+			//var name = Console.ReadLine();
+			//var array = new char[name.Length];
+
+
+			//for (var i = name.Length; i > 0; i--)
+			//{
+			//	array[name.Length - i] = name[i - 1];
+			//}
+			//var reversed = new string(array);
+
+			//Console.WriteLine("Reversed name: " + reversed);
+
+
+
+			Console.Write("What is your name: ");
+			var name = Console.ReadLine().ToCharArray();
+
+            Array.Reverse(name);
+
+            var reverse = new string(name);
+
+			Console.WriteLine("Reversed name: " + reverse);
+
+		}
+    }
+}
---
+++ Exercises/ListsArraysExercise4/ListsArraysExercise4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListsArraysExercise4
+{
+    class Program
+    {   
+		/// <summary>
+        /// Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may 
+        /// include duplicates. Display the unique numbers that the user has entered.
+        /// </summary>
+
+        static void Main(string[] args)
+        {
+			var numbers = new List<int>();
+
+            while(true)
+			{
+				Console.Write("Enter a number (or 'Quit' to exit): ");
+				var input = Console.ReadLine();
+
+				if (input.ToLower() == "quit" || String.IsNullOrWhiteSpace(input))
+                    break;
+
+				numbers.Add(Convert.ToInt32(input));
+			}
+
+
+			var uniques = new List<int>();
+
+            foreach (var number in numbers)
+			{
+				if (!uniques.Contains(number))
+					uniques.Add(number);
+			}
+
+			Console.WriteLine("Unqiue Numbers: ");
+            foreach (var number in uniques)
+				Console.WriteLine(number);
+		}
+    }
+}
