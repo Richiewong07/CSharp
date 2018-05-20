70c634b9e4fc946ba0424b57c367e89c940e9377
Richie Wong <richiewong07@yahoo.com>
2018-05-19 19:44:25 -0500

    new

---
+++ Dates/DateTime/DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Datetime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var datetime = new DateTime(2018, 5, 20);
+			var now = DateTime.Now;
+			var today = DateTime.Today;
+
+			//Console.WriteLine("Hour: " + now.Hour);
+			//Console.WriteLine("Minute: " + now.Minute);
+
+			var tomorrow = now.AddDays(1);
+			var yesterday = now.AddDays(-1);
+
+			Console.WriteLine("To Long Date String: " + now.ToLongDateString());
+			Console.WriteLine("To Short Date String: " + now.ToShortDateString());
+			Console.WriteLine("To Long Time String: " + now.ToLongTimeString());
+			Console.WriteLine("To Short Time String: " + now.ToShortTimeString());
+			Console.WriteLine("To Both Date and Time: " + now.ToString("yyyy-MM-dd HH:mm"));
+		}
+    }
+}
--- Exercises/ListsArraysExercise5/ListsArraysExercise5/Program.cs
+++ Exercises/ListsArraysExercise5/ListsArraysExercise5/Program.cs
@@ -5,7 +5,12 @@ namespace ListsArraysExercise5
 {
     class Program
     {
-		      
+		/// <summary>
+        /// Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is 
+        /// empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display 
+        /// the 3 smallest numbers in the list.
+        /// </summary>
+
         static void Main(string[] args)
         {
 			string[] elements;
