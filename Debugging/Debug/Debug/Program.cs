fc472c12ace78c712712693bba5f797002a72de2
Richie Wong <richiewong07@yahoo.com>
2018-05-25 18:12:50 -0500

    updated

--- Debugging/Debug/Debug/Program.cs
+++ Debugging/Debug/Debug/Program.cs
@@ -7,7 +7,7 @@ namespace Debug
     {
         public static void Main(string[] args)
         {
-			var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+			var numbers = new List<int> { 1, 2 };
 			var smallests = GetSmallests(numbers, 3);
             
             foreach(var number in smallests)
@@ -17,6 +17,11 @@ namespace Debug
         
         public static List<int> GetSmallests(List<int> list, int count)
 		{
+			if (list == null)
+				throw new ArgumentNullException("list");
+			if (count > list.Count || count <= 0)
+				throw new ArgumentOutOfRangeException("Count should be between 1 and the number of elements in the list.");
+			
 			var buffer = new List<int>(list);
 			var smallests = new List<int>();
             while (smallests.Count < count)
@@ -29,7 +34,7 @@ namespace Debug
 			
 		}
 
-
+       
         public static int GetSmallest(List<int> list)
 		{
 			var min = list[0];
