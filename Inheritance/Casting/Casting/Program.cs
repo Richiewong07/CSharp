224732309313618c4ad4cd3704664c3065245bc2
Richie Wong <richiewong07@yahoo.com>
2018-06-09 15:54:12 -0500

    updated

---
+++ Inheritance/Casting/Casting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Casting
+{
+	class Program
+    {
+		static void Main(string[] args)
+		{
+
+			// UPCASTING
+			//Text text = new Text();
+			//Shape shape = text;
+           
+
+            // DOWNCASTING
+			Shape shape = new Text();
+			if (shape is Text)
+			{
+				Text text = (Text)shape;
+			}
+
+
+
+		}
+    }
+}
---
+++ Inheritance/Constructors/Constructors/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+			: base(registrationNumber)
+        {
+			Console.WriteLine("Car is being initiazlied. {0}", registrationNumber);
+        }
+    }
+}
